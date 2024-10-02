7282c1aa7979b745eca84d581130bc6d8edb39ea
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-10-02 13:25:38 +0300

    product details comments

---
+++ src/components/product-details/ProductInfo.jsx
@@ -0,0 +1,65 @@
+import { useTranslation } from "react-i18next";
+
+function ProductInfo({ product }) {
+  const { t } = useTranslation();
+
+  const handleShare = () => {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: product?.name,
+          text: product?.description,
+          url: window.location.href
+        })
+        .then(() => console.log("Shared successfully"))
+        .catch((error) => console.log("Error sharing:", error));
+    } else {
+      alert(t("share_not_supported"));
+    }
+  };
+
+  return (
+    <>
+      <div className="priceInfo mt-3">
+        <div className="price">
+          <span>
+            {product?.price} {product?.currency?.name}
+          </span>
+        </div>
+
+        <button className={`favorite ${product?.is_favorite ? "active" : ""}`}>
+          <i className="fa-light fa-heart"></i>
+        </button>
+
+        <div className="actions">
+          <span className="action-btn report" onClick={handleShare}>
+            <i className="fa-solid fa-share"></i> {t("share")}
+          </span>
+
+          <span className="action-btn report">
+            <i className="fa-regular fa-flag"></i> {t("report")}
+          </span>
+        </div>
+      </div>
+
+      <div className="itemInfo mt-3">
+        <h3 className="title">{product?.name}</h3>
+
+        <div className="itemBottom">
+          <div className="location">
+            <i className="fa-light fa-location-dot"></i>
+            <span>
+              {product?.country?.name}, {product?.city?.name}
+            </span>
+          </div>
+          <div className="time">
+            <i className="fa-light fa-clock"></i> {product?.create_at}
+          </div>
+        </div>
+        <p className="description">{product?.description}</p>
+      </div>
+    </>
+  );
+}
+
+export default ProductInfo;
--- src/routes/ProductDetails.jsx
+++ src/routes/ProductDetails.jsx
@@ -1,20 +1,27 @@
 import ProductSlider from "../components/product-details/ProductSlider";
 import useGetProduct from "../hooks/products/useGetProduct";
 import UserCard from "../components/product-details/UserCard";
+import ProductInfo from "../components/product-details/ProductInfo";
+import Comments from "../components/product-details/Comments";
+import PageLoader from "../ui/loaders/PageLoader";
 
 function ProductDetails() {
-  const { data: product } = useGetProduct();
+  const { data: product, isLoading } = useGetProduct();
 
-  return (
+  return isLoading ? (
+    <PageLoader />
+  ) : (
     <section className="product_details">
       <div className="container">
         <div className="row m-0">
           <div className="col-lg-7 col-12 p-lg-3 p-2">
             <ProductSlider product={product} />
+            <ProductInfo product={product} />
           </div>
           <div className="col-lg-5 col-12 p-lg-3 p-2 ">
-            <div className="d-flex flex-column gap-3">
+            <div className="d-flex flex-column gap-4">
               <UserCard product={product} />
+              <Comments product={product} />
             </div>
           </div>
         </div>
