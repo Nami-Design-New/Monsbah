7baf04c74eafc75c14bbc64a950a5d518aebd904
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-10-10 10:48:38 +0300

    delete chats

--- src/routes/Chats.jsx
+++ src/routes/Chats.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useRef, useState } from "react";
 import { Form } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import { toast } from "react-toastify";
+import { useQueryClient } from "@tanstack/react-query";
 import useGetChats from "../hooks/chat/useGetChats";
 import ChatCard from "../ui/cards/ChatCard";
 import ChatItemLoader from "../ui/loaders/ChatItemLoader";
+import ConfirmationModal from "../ui/modals/ConfirmationModal";
+import axiosInstance from "../utils/axiosInstance";
 
 function Chats() {
   const { t } = useTranslation();
-  const [checkedState, setCheckedState] = useState(false);
-  const [selectedChats, setSelectedChats] = useState([]);
   const sideRef = useRef(null);
+  const queryClient = useQueryClient();
+  const [checkedState, setCheckedState] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState();
+  const [selectedChats, setSelectedChats] = useState([]);
 
   const {
     data: chats,
@@ -50,6 +57,27 @@ function Chats() {
     return arr1.every((val) => selectedChats.includes(val));
   };
 
+  const handleDelete = async () => {
+    setLoading(true);
+    try {
+      const res = await axiosInstance.post("/client/chat/delete", {
+        ids: selectedChats,
+      });
+      if (res.status === 200) {
+        toast.success(res.data.message);
+        queryClient.invalidateQueries({ queryKey: ["chats"] });
+        setSelectedChats([]);
+        setCheckedState(false);
+        setShowModal(false);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <section className="chats-section">
       <div className="container">
@@ -68,9 +96,12 @@ function Chats() {
                     {!areArraysEqual() && (
                       <button onClick={selectAll}>{t("chat.selectAll")}</button>
                     )}
-                    <button className="delete_all">
+                    <button
+                      className="delete_all"
+                      onClick={() => setShowModal(true)}
+                    >
                       <i className="fa-regular fa-trash" aria-hidden="true"></i>
-                      {t("chat.deleteAll")}
+                      {t("chat.delete")}
                     </button>
                   </div>
                 )}
@@ -93,6 +124,15 @@ function Chats() {
           <div className="col-lg-8 col-12 p-2"></div>
         </div>
       </div>
+      <ConfirmationModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        eventFun={handleDelete}
+        loading={loading}
+        type="delete"
+        buttonText={t("chat.delete")}
+        text={t("chat.areYouSureYouWantDeleteThisChats")}
+      />
     </section>
   );
 }
