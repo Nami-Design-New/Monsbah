e63f204f230ebbaa186ffe46547f99b0a3d73ddf
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-10-14 15:09:12 +0300

    comments tree

--- src/ui/cards/CommentCard.jsx
+++ src/ui/cards/CommentCard.jsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-function CommentCard({ comment, deleteComment }) {
+function CommentCard({ comment, deleteComment, setTargetComment }) {
   const { t } = useTranslation();
   const authedUser = useSelector((state) => state.clientData.client);
 
@@ -23,43 +23,34 @@ function CommentCard({ comment, deleteComment }) {
           </div>
           <div className="actions">
             <span>{comment?.date}</span>
-            <button>{t("reply")}</button>
+
+            <button onClick={() => setTargetComment(comment)}>
+              {t("reply")}
+            </button>
+
             {comment?.user_id === authedUser?.id && (
               <button onClick={() => deleteComment(comment?.id)}>
                 {t("delete")}
               </button>
             )}
+
             <button>{t("report")}</button>
           </div>
         </div>
       </div>
-      <div className="replies">
-        <div className="CommentCard">
-          <Link to={`/profile/${comment?.user_id}`} className="img">
-            <img
-              src={comment?.user_image}
-              alt={comment?.user_name}
-              onError={(e) => (e.target.src = "/images/icons/user_default.png")}
+
+      {comment?.replies?.length > 0 && (
+        <div className="replies">
+          {comment.replies.map((reply) => (
+            <CommentCard
+              key={reply.id}
+              comment={reply}
+              setTargetComment={setTargetComment}
+              deleteComment={deleteComment}
             />
-          </Link>
-          <div className="content">
-            <h6>{comment?.user_name}</h6>
-            <div className="comment">
-              <p>{comment?.comment}</p>
-            </div>
-            <div className="actions">
-              <span>{comment?.date}</span>
-              <button>{t("reply")}</button>
-              {comment?.user_id === authedUser?.id && (
-                <button onClick={() => deleteComment(comment?.id)}>
-                  {t("delete")}
-                </button>
-              )}
-              <button>{t("report")}</button>
-            </div>
-          </div>
+          ))}
         </div>
-      </div>
+      )}
     </div>
   );
 }
