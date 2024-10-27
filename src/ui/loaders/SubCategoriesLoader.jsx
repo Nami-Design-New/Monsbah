ce0cbfeaae9c13fc9d77bcea6ab25621b6c37a5f
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-10-27 15:08:21 +0200

    updates

--- src/ui/loaders/SubCategoriesLoader.jsx
+++ src/ui/loaders/SubCategoriesLoader.jsx
@@ -1,8 +1,8 @@
 export default function SubCategoriesLoader() {
   return (
     <div className="category sub skeleton">
-      <div className="image-wrapper "></div>
-      <h6></h6>
+      <div className="image-wrapper skeleton-img"></div>
+      <h6 className="skeleton-text skeleton-title"></h6>
     </div>
   );
 }
