40b72b4033c7172f7873e0089d93be48fe008467
Ahmed Abd El-Ghany <ahmed.abdelghany1211@gmail.com>
2024-10-27 16:05:14 +0300

    updates

--- src/components/profile/RatesTab.jsx
+++ src/components/profile/RatesTab.jsx
@@ -15,37 +15,37 @@ function RatesTab({ user, isActive }) {
 
   return (
     <section className="products_section w-100">
-        <div className="w-100">
-          <div className=" w-100 actions-wrapper p-0 d-flex align-items-center justify-content-end">
-            <span
-              className="customBtn m-0 d-flex align-items-center gap-2"
-              style={{ cursor: "pointer" }}
-              onClick={() => setShowModal(true)}
-            >
-              <i className="fa-regular fa-star"></i>
-              {t("rate")}
-            </span>
+      <div className="w-100">
+        <div className=" w-100 actions-wrapper p-0 d-flex align-items-center justify-content-end">
+          <span
+            className="customBtn m-0 d-flex align-items-center gap-2"
+            style={{ cursor: "pointer" }}
+            onClick={() => setShowModal(true)}
+          >
+            <i className="fa-regular fa-star"></i>
+            {t("rate")}
+          </span>
+        </div>
+      </div>
+      <div className="row">
+        {rates?.data?.data?.data?.map((rate, index) => (
+          <div className="col-12 p-2" key={index}>
+            <RateCard rate={rate} userId={user?.id} className="reverse" />
           </div>
-        </div>
-        <div className="row">
-          {rates?.data?.data?.data?.map((rate, index) => (
-            <div className="col-12 p-2" key={index}>
-              <RateCard rate={rate} userId={user?.id} className="reverse" />
-            </div>
-          ))}
+        ))}
 
-          {isLoading && (
-            <>
-              {Array(3)
-                .fill(0)
-                .map((_, index) => (
-                  <div className="col-lg-6 col-12 p-2" key={`loader-${index}`}>
-                    <AskLoader />
-                  </div>
-                ))}
-            </>
-          )}
-        </div>
+        {isLoading && (
+          <>
+            {Array(3)
+              .fill(0)
+              .map((_, index) => (
+                <div className="col-lg-6 col-12 p-2" key={`loader-${index}`}>
+                  <AskLoader />
+                </div>
+              ))}
+          </>
+        )}
+      </div>
       <CreateRateModal
         id={user?.id}
         showModal={showModal}
